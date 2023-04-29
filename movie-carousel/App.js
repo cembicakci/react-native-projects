425803211759ae0f4b61fc4fec97ee19380f1591
Cem Bıçakcı <cmbicakci@gmail.com>
2023-04-29 17:53:59 +0300

    fetch datas from movies api

--- movie-carousel/App.js
+++ movie-carousel/App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StatusBar, Text, View, StyleSheet, FlatList, Dimensions, Image, Animated } from 'react-native';
+import { getMovies } from './api';
+import Genres from './Genres';
+import Rating from './Rating';
+
+const { width, height } = Dimensions.get('window');
+
+const SPACING = 10;
+const ITEM_SIZE = width * 0.72;
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <Text style={styles.paragraph}>Loading...</Text>
+  </View>
+);
+
 
 export default function App() {
+  const [movies, setMovies] = React.useState([]);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const movies = await getMovies();
+      setMovies(movies);
+    };
+
+    if (movies.length === 0) {
+      fetchData(movies);
+    }
+  }, [movies]);
+
+  if (movies.length === 0) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={(item) => item.key}
+        horizontal
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={{ width: ITEM_SIZE }}>
+              <View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING * 2,
+                  alignItems: 'center',
+                  backgroundColor: 'white',
+                  borderRadius: 34,
+                }}
+              >
+                <Image
+                  source={{ uri: item.poster }}
+                  style={styles.posterImage}
+                />
+                <Text style={{ fontSize: 24 }} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <Rating rating={item.rating} />
+                <Genres genres={item.genres} />
+                <Text style={{ fontSize: 12 }} numberOfLines={3}>
+                  {item.description}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  posterImage: {
+    width: '100%',
+    height: ITEM_SIZE * 1.2,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
   },
 });
