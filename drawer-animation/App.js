7d75bbf27a503c2d2ef0cd7f992612118ae8f1cc
Cem Bıçakcı <cmbicakci@gmail.com>
2023-05-04 12:15:24 +0300

    added masked view

--- drawer-animation/App.js
+++ drawer-animation/App.js
@@ -1,8 +1,16 @@
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Animated } from 'react-native';
+import Svg, { Polygon } from 'react-native-svg'
+
 import { AntDesign } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
+
 import { colors, links, routes } from './utils';
-import React from 'react';
+import MaskedView from '@react-native-community/masked-view';
+
+const { width, height } = Dimensions.get('screen')
+
+const AnimatedPolygon = Animated.createAnimatedComponent(Polygon)
 
 const Button = ({ title, onPress, style }) => {
   return (
@@ -12,10 +20,34 @@ const Button = ({ title, onPress, style }) => {
   );
 };
 
+const fromCoords = { x: 0, y: height }
+const toCoords = { x: width, y: 0 }
 
-const Drawer = ({ onPress }) => {
+const Drawer = ({ onPress, animatedValue }) => {
+  const polygonRef = React.useRef()
+
+  React.useEffect(() => {
+    animatedValue.addListener((v) => {
+      if (polygonRef?.current) {
+        polygonRef.current.setNativeProps({
+          points: `0,0 ${v.x},${v.y} ${width},${height} 0,${height}`
+        })
+      }
+    })
+  })
   return (
-    <View style={{ flex: 1 }}>
+    <MaskedView
+      style={{ flex: 1 }}
+      maskElement={
+        <Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+          <AnimatedPolygon
+            ref={polygonRef}
+            fill={'red'}
+            points={`0,0 ${fromCoords.x},${fromCoords.y} ${width},${height} 0,${height}`}
+          />
+        </Svg>
+      }
+    >
       <View style={styles.menuContainer}>
         <AntDesign
           onPress={onPress}
@@ -59,24 +91,32 @@ const Drawer = ({ onPress }) => {
         </View>
 
       </View>
-
-    </View>
+    </MaskedView>
   )
 }
 
 export default function App() {
 
+  const animatedValue = React.useRef(new Animated.ValueXY(fromCoords)).current
+  const animate = (toValue) => {
+    return Animated.timing(animatedValue, {
+      toValue: toValue === 1 ? toCoords : fromCoords,
+      duration: 2000,
+      useNativeDriver: true
+    })
+  }
+
   const onCloseDrawer = React.useCallback(() => {
 
-  })
+  }, [])
 
   const onOpenDrawer = React.useCallback(() => {
-
-  })
+    animate(1).start()
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Drawer onPress={onCloseDrawer} />
+      <Drawer onPress={onCloseDrawer} animatedValue={animatedValue} />
       <AntDesign
         onPress={onOpenDrawer}
         name='menufold'
