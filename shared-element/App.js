7405fb159ca2ff23d6bf3c4b36db8d77f1c5cda3
Cem Bıçakcı <cmbicakci@gmail.com>
2023-05-06 14:10:18 +0300

    added react navigation

--- shared-element/App.js
+++ shared-element/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import SalonList from './src/SalonList';
+import SalonListDetail from './src/SalonListDetail';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <SalonList />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SalonList" component={SalonList} />
+        <Stack.Screen name="SalonListDetail" component={SalonListDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
--- shared-element/src/SalonList.js
+++ shared-element/src/SalonList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Dimensions, SafeAreaView } from 'react-native'
 import { data } from './data'
 import { colors } from './colors'
 
@@ -7,16 +7,17 @@ const { width, height } = Dimensions.get('screen')
 
 const ITEM_HEIGHT = height * 0.18
 
-const SalonList = () => {
+const SalonList = ({ navigation }) => {
+
     return (
-        <View style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
             <FlatList
                 data={data}
                 keyExtractor={(item) => item.name.first}
                 contentContainerStyle={{ padding: 10 }}
                 renderItem={({ item, index }) => {
                     return (
-                        <TouchableOpacity onPress={() => { }} style={{ marginBottom: 10, height: ITEM_HEIGHT }}>
+                        <TouchableOpacity onPress={() => { navigation.navigate('SalonListDetail') }} style={{ marginBottom: 10, height: ITEM_HEIGHT }}>
                             <View style={{ flex: 1, padding: 10 }}>
                                 <View style={[StyleSheet.absoluteFillObject, { backgroundColor: colors[index], borderRadius: 16 }]} />
                                 <Text style={styles.name}>{item.name.first} {item.name.last}</Text>
@@ -27,7 +28,7 @@ const SalonList = () => {
                     )
                 }}
             />
-        </View>
+        </SafeAreaView>
     )
 }
 
