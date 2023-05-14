c249eb406833e7244281b7ff799c3ad5446a327b
Cem Bıçakcı <cmbicakci@gmail.com>
2023-05-14 14:15:10 +0300

    animated carousel

--- carousel-animation/App.js
+++ carousel-animation/App.js
@@ -8,18 +8,59 @@ const imageW = width * 0.7;
 const imageH = imageW * 1.54;
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current
 
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <FlatList
+      <View style={[StyleSheet.absoluteFillObject]}>
+        {
+          data.map((image, index) => {
+            const inputRange = [
+              (index - 1) * width, //prev
+              index * width, //curr
+              (index + 1) * width //next
+            ]
+
+            //display the current image only when the index calls the own image
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0, 1, 0]
+            })
+            return (
+              <Animated.Image
+                source={{ uri: image }}
+                key={index}
+                style={[StyleSheet.absoluteFillObject, { opacity }]}
+                blurRadius={50}
+              />
+            )
+          })
+        }
+      </View>
+      <Animated.FlatList
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
         data={data}
         keyExtractor={(_, index) => index.toString()}
         horizontal
         pagingEnabled
         renderItem={({ item }) => {
           return (
-            <View style={{ width, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{
+              width,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowColor: '#000',
+              shadowOpacity: 0.5,
+              shadowOffset: {
+                width: 0,
+                height: 0
+              },
+              shadowRadius: 20
+            }}>
               <Image
                 source={{ uri: item }}
                 style={{
