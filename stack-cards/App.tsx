5f6b80f3cda1b4bed80206a3743507786247f7f5
Cem Bıçakcı <cmbicakci@gmail.com>
2023-06-28 22:07:08 +0300

    flip left, right

--- stack-cards/App.tsx
+++ stack-cards/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, Image, Alert } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, Alert, Pressable } from 'react-native';
 
 import { Entypo } from '@expo/vector-icons'
 import { Directions, Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler'
-import Animated, { Extrapolate, SharedValue, interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { Extrapolate, FadeIn, FadeInRight, FadeOut, FadeOutRight, SharedValue, interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 import data, { locationImage } from './data';
+import { useState } from 'react';
 
 const { width } = Dimensions.get('window')
 const duration = 300
@@ -96,32 +97,124 @@ function Card({ info, index, totalLength, activeIndex }: { info: (typeof data)[0
   )
 }
 
+const menu = ['Home', 'About', 'Contact', 'Settings', 'Logout']
+function Menu({
+  menu,
+  activeMenuIndex = 0,
+  onClose,
+  onMenuPress,
+  isMenuVisible
+}: {
+  menu: string[],
+  activeMenuIndex?: number,
+  onClose: () => void,
+  onMenuPress: (index: number) => void
+  isMenuVisible: boolean
+}) {
+
+  if (!isMenuVisible) {
+    return null;
+  }
+
+  return (
+    <Animated.View
+      entering={FadeIn}
+      exiting={FadeOut.delay(500)}
+      style={[StyleSheet.absoluteFillObject, { zIndex: 999999 }]}
+      pointerEvents={'box-none'}
+    >
+      <Pressable onPress={onClose} style={StyleSheet.absoluteFillObject}>
+        <View style={{ backgroundColor: colors.dark, opacity: 0.75, flex: 1 }}></View>
+      </Pressable>
+      <View style={{
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        padding: layout.cardsGap * 2,
+        gap: layout.spacing
+      }}
+        pointerEvents='box-none'
+      >
+        {
+          menu.map((item, index) => {
+            return (
+              <Pressable onPress={() => { onMenuPress(index) }} key={index}>
+                <Animated.Text
+                  entering={FadeInRight.delay(50 * index)}
+                  exiting={FadeOutRight.delay((menu.length) * 50)}
+                  style={{
+                    fontSize: 32,
+                    fontWeight: 'bold',
+                    color: colors.light
+                  }}
+                >
+                  {index === activeMenuIndex ? '👉🏻' : ''}
+                  {item}
+                </Animated.Text>
+              </Pressable>
+            )
+          })
+        }
+      </View>
+
+    </Animated.View>
+
+  )
+}
+
 
 export default function App() {
 
-  const activeIndex = useSharedValue(0)
+  const floatActiveIndex = useSharedValue(0)
+
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+
+  console.log(isMenuVisible)
 
   const flingUp = Gesture.Fling().direction(Directions.UP).onStart(() => {
-    if (activeIndex.value === 0) {
+    if (floatActiveIndex.value === 0) {
       return;
     }
-    activeIndex.value = withTiming(activeIndex.value - 1, { duration })
+    floatActiveIndex.value = withTiming(floatActiveIndex.value - 1, { duration })
     console.log('fling up')
   })
 
   const flingDown = Gesture.Fling().direction(Directions.DOWN).onStart(() => {
-    if (activeIndex.value === data.length) {
+    if (floatActiveIndex.value === data.length) {
       return;
     }
-    activeIndex.value = withTiming(activeIndex.value + 1, { duration })
+    floatActiveIndex.value = withTiming(floatActiveIndex.value + 1, { duration })
     console.log('fling down')
+  })
+
+  const flingRight = Gesture.Fling().direction(Directions.RIGHT).onStart(() => {
+    'worklet'
+    runOnJS(setIsMenuVisible)(false)
+
+  })
+
+  const flingLeft = Gesture.Fling().direction(Directions.LEFT).onStart(() => {
+    'worklet'
+    runOnJS(setIsMenuVisible)(true)
   })
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar hidden />
+
+      <Menu
+        menu={menu}
+        activeMenuIndex={activeIndex}
+        onMenuPress={(index) => {
+          setIsMenuVisible(false)
+        }}
+        onClose={() => { setIsMenuVisible(false) }}
+        isMenuVisible={isMenuVisible}
+      />
+
       <GestureDetector
-        gesture={Gesture.Exclusive(flingUp, flingDown)}
+        gesture={Gesture.Exclusive(flingUp, flingDown, flingRight, flingLeft)}
       >
         <View style={{
           alignItems: 'center',
@@ -140,7 +233,7 @@ export default function App() {
                   key={c.id}
                   index={index}
                   totalLength={data.length - 1}
-                  activeIndex={activeIndex}
+                  activeIndex={floatActiveIndex}
                 />
               )
             })
