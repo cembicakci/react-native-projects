484228faa29c19425063bcc7d5d6ce11faa083b4
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-27 11:07:19 +0300

    picker

---
+++ picker/App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
